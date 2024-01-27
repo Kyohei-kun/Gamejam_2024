09cf02b5c57ff545c43c566f2fd006cebb09da30
Cedric_Gazzano <cedric.gazzano@gmail.com>
2024-01-27 11:40:25 +0100

    [Code] Add victory quest system

---
+++ Assets/Scripts/CS_QuestSystem.cs
@@ -0,0 +1,34 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class CS_QuestSystem : MonoBehaviour
+{
+    [SerializeField] VisualEffect effect;
+
+    private void Start()
+    {
+        QuestSystem.Init();
+    }
+
+    public void PlayVictoryFX()
+    {
+        effect.Play();
+    }
+}
+
+public static class QuestSystem
+{
+    static CS_QuestSystem currentQuestSystem;
+    static public void Init()
+    {
+        currentQuestSystem = GameObject.FindGameObjectWithTag("Player").GetComponent<CS_QuestSystem>();
+    }
+
+    public static void PlayVictoryFX()
+    {
+        currentQuestSystem.PlayVictoryFX();
+    }
+}
