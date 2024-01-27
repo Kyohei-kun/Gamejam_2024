fc4612f5935cfb49bc22b730cf10abaf0a670c59
Cedric_Gazzano <cedric.gazzano@gmail.com>
2024-01-27 17:05:15 +0100

    [Code] flower quest

---
+++ Assets/Quests/Flower/CS_Flower.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_Flower : MonoBehaviour, CS_I_Fart
+{
+    [SerializeField] GameObject normalModel;
+    [SerializeField] GameObject fannedModel;
+
+    private CS_FlowerQuest flowerQuest;
+
+    bool state = false;
+
+    private void Start()
+    {
+        FartMagazin.AddAbo(this, gameObject, 1);
+    }
+
+    public void SetFlowerQuest(CS_FlowerQuest quest)
+    {
+        flowerQuest = quest;
+    }
+
+    public void FartInteract()
+    {
+        if (state != true)
+        {
+            state = true;
+            flowerQuest.OnFannedFlower(transform.position);
+            normalModel.SetActive(false);
+            fannedModel.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Quests/Flower/CS_FlowerQuest.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_FlowerQuest : MonoBehaviour
+{
+    [SerializeField] List<CS_Flower> flowers;
+
+    int currentFlowerFanned = 0;
+
+    public void OnFannedFlower(Vector3 flowerPos)
+    {
+        currentFlowerFanned++;
+        if(currentFlowerFanned >= flowers.Count)
+        {
+            QuestSystem.PlayVictoryFX(flowerPos);
+        }
+    }
+
+    private void Start()
+    {
+        foreach (var flower in flowers)
+        {
+            flower.SetFlowerQuest(this);
+        }
+    }
+}
