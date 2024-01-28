2c346f9a6562f3ff7a67251cf7181644679b8123
Cedric_Gazzano <cedric.gazzano@gmail.com>
2024-01-28 16:17:31 +0100

    Son cry

--- Assets/Quests/BB Pouic/CS_BBPouic.cs
+++ Assets/Quests/BB Pouic/CS_BBPouic.cs
@@ -7,11 +7,14 @@ public class CS_BBPouic : MonoBehaviour
     [SerializeField] GameObject fxPleur;
     [SerializeField] GameObject fx;
 
+    [SerializeField] AudioSource audioSource;
+
     public void BallonDrop()
     {
         Destroy(fxPleur);
         fx.SetActive(true);
         QuestSystem.PlayVictoryFX(transform.position);
         CS_QuestUISystem.Validate(6);
+        Destroy(audioSource);
     }
 }
